98ddec415e4c080b1f58808178063c2e979b3e88
guiguibg <guillaume.jacquin@epitech.eu>
2021-11-29 12:59:01 +0100

    [add] Boucle back

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import paho.mqtt.client as paho  		    #mqtt library
+import os
+import json
+import time
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +23,8 @@ def light_sensor():
         print(is_open)
         print(time)
         
+        test1("OJfqGEBS6iy8OTv6R7vu")
+
         return(result)
    
 
@@ -46,3 +54,38 @@ def fluid_sensor():
     print(color)
     
     return(result)
+
+
+
+
+
+broker="thingsboard.cloud"   			    #host name
+port=1883 					    #data listening port
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published to thingsboard \n")
+    pass
+
+def test1(ACCESS_TOKEN):
+    client1= paho.Client("control1")
+    client1.on_publish = on_publish
+    client1.username_pw_set(ACCESS_TOKEN)
+    client1.connect(broker,port,keepalive=60)
+    
+    test = 0
+
+    while test < 1:
+        rand_humidity = (random.randint(0, 100))
+        rand_temp = (random.randint(-20, 50))
+
+        string1 = "\"Humidity\":" + str(rand_humidity) + ","
+        string2 = "\"Temperature\":" + str(rand_temp)
+        payload="{"
+        payload+=string1
+        payload+=string2
+        payload+="}"
+        ret= client1.publish("v1/devices/me/telemetry",payload)
+        print("Please check LATEST TELEMETRY field of your device")
+        print(payload)
+        time.sleep(1)
+        test +=1
