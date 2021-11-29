3e55ead75f56717c007cc4f239d5a7eb88ad5a18
guiguibg <guillaume.jacquin@epitech.eu>
2021-11-29 11:22:26 +0100

    [add] server

--- app.py
+++ app.py
@@ -2,48 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/test')
-def test():
-    return("NIQUE TA MERE")
-
-    
-@app.route('/json-example', methods=['POST'])
-def json_example():
-    request_data = request.get_json()
-    print("ajisojao")
-    print(request_data)
-
-    language = None
-    framework = None
-    python_version = None
-    example = None
-    boolean_test = None
-
-    if request_data:
-        if 'language' in request_data:
-            language = request_data['language']
-
-        if 'framework' in request_data:
-            framework = request_data['framework']
-
-        if 'version_info' in request_data:
-            if 'python' in request_data['version_info']:
-                python_version = request_data['version_info']['python']
-
-        if 'examples' in request_data:
-            if (type(request_data['examples']) == list) and (len(request_data['examples']) > 0):
-                example = request_data['examples'][0]
-
-        if 'boolean_test' in request_data:
-            boolean_test = request_data['boolean_test']
-
-    return '''
-           The language value is: {}
-           The framework value is: {}
-           The Python version is: {}
-           The item at index 0 in the example list is: {}
-           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
-
 
 @app.route('/light_sensor', methods=['POST'])
 def light_sensor():
@@ -71,7 +29,7 @@ def temp_humidity():
     return(result)
 
 @app.route('/fluid_sensor', methods=['POST'])
-def temp_humidity():
+def fluid_sensor():
     result = request.form
     value = result["value"]
     time = result['time']
